4d468f2e5e7253f798c76e20a670dfb6083fabf6
Jana Ahurtsova <byjankaaa@gmail.com>
2022-12-12 16:57:30 +0300

    feat: add some olutions to functions

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -162,8 +162,13 @@ function doRectanglesOverlap(/* rect1, rect2 */) {
  *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
  *
  */
-function isInsideCircle(/* circle, point */) {
-  throw new Error('Not implemented');
+function isInsideCircle(circle, point) {
+  const points = ((point.x - circle.center.x) ** 2) + ((point.y - circle.center.y) ** 2);
+  const radius = circle.radius ** 2;
+  if (points < radius) {
+    return true;
+  }
+  return false;
 }
 
 
@@ -413,6 +418,7 @@ function getCommonDirectoryPath(/* pathes */) {
   // const str = '';
 
   // return str;
+  throw new Error('Not implemented');
 }
 
 
